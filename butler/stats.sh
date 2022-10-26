096cd431c3b8e8ac4a0cb7b26411e82eb7b3b47a
Zed <ztec@riper.fr>
2022-10-27 00:30:31 +0200

    Remove kube-probe from stats

--- butler/stats.sh
+++ butler/stats.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-cat /riper/kube/blog.ztec.fr/logs/access*.log |grep -v ping |grep -v uptimerobot |grep -v StatusCake |goaccess \
--o stats.html \
+cat /riper/kube/blog.ztec.fr/logs/access*.log |grep -v ping \
+ |grep -v uptimerobot |grep -v StatusCake | grep -v kube-probe \
+ > /tmp/all.log
+
+goaccess /tmp/all.log \
 --log-format='%^ %^[%d:%t %^] "%r" %s %b"%R" "%u" "%h" %^ %^ "%T" %^ ' \
 --date-format=%d/%b/%Y --time-format=%T \
 --anonymize-ip --exclude-ip 10.0.0.0-10.25.255.255 \
@@ -9,4 +12,4 @@ cat /riper/kube/blog.ztec.fr/logs/access*.log |grep -v ping |grep -v uptimerobot
 --hide-referrer="blog.ztec.fr" \
 --ignore-panel "KEYPHRASES" \
 --date-spec=hr \
---geoip-database /home/zed/geoip/dbip-city-lite-2021-04.mmdb -
+--geoip-database /home/zed/geoip/dbip-city-lite-2021-04.mmdb
