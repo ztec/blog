cdf834a38a90e7a09b8754c2c6dba99c2a9571d7
Zed <ztec@riper.fr>
2022-10-28 10:40:34 +0200

    test-fix sync

--- butler/syncFromGithub.sh
+++ butler/syncFromGithub.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-git remote add riper ssh://git@git.riper.fr:22023/ztec/blog.git
-git remote add github git@github.com:ztec/blog.git
+## Need to execute syncToGithubFirst
 git fetch riper
 git fetch github
 git push riper HEAD:main
