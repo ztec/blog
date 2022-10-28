3854f305478f526043a3adcb5ce4e7c4a77d9ae5
Zed <ztec@riper.fr>
2022-10-28 10:33:14 +0200

    test-fix sync

--- butler/syncToGithub.sh
+++ butler/syncToGithub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git remote rename origin riper
+git remote add riper ssh://git@git.riper.fr:22023/ztec/blog.git
 git remote add github git@github.com:ztec/blog.git
 git fetch riper
 git fetch github
